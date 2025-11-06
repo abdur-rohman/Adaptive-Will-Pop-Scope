876cfb75f55e4fe27a2998360ae92ba7f5223407
Abdur Rohman <abdur.rohman2883@gmail.com>
2025-11-06 14:54:33 +0700

    fix: Apple devices can still use the swipe back gesture update: Example project to match the latest package version

---
+++ example/lib/third_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import 'package:adaptive_will_pop_scope/adaptive_will_pop_scope.dart';
+
+class ThirdPage extends StatefulWidget {
+  const ThirdPage({super.key});
+
+  static MaterialPageRoute get route => MaterialPageRoute(
+        builder: (_) => const ThirdPage(),
+      );
+
+  @override
+  State<ThirdPage> createState() => ThirdPageState();
+}
+
+class ThirdPageState extends State<ThirdPage> {
+  @override
+  Widget build(BuildContext context) => AdaptiveWillPopScope(
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Third Page'),
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          ),
+          body: const Center(child: Text('Hello World')),
+        ),
+      );
+}
